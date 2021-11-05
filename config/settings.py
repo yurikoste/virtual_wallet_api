a0e137aacf68fc4adf0b7b030a7e0bdab44b0308
Yurii Kostenko <kostenko.yuriy@gmail.com>
2021-11-05 15:36:58 +0200

    Auth updates

--- config/settings.py
+++ config/settings.py
@@ -102,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
   'DEFAULT_AUTHENTICATION_CLASSES': (
+    'rest_framework.authentication.SessionAuthentication',
     'rest_framework_simplejwt.authentication.JWTAuthentication',
   ),
 }
