7db2d854f35db624ccdfbbf169167f9232743cde
Yurii Kostenko <kostenko.yuriy@gmail.com>
2021-11-11 00:06:25 +0200

    Business logic was moved to serializers

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,6 +1,10 @@
+from django.db.transaction import atomic
 from rest_framework import serializers
-from .models import Transaction, Wallet
+
 from decimal import Decimal, getcontext
+
+from .models import Transaction, Wallet
+from user.models import VirtualWalletUser
 
 from pprint import pprint
 
@@ -12,9 +16,25 @@ class FillSerializer(serializers.ModelSerializer):
         model = Transaction
         fields = ('value',)
 
-    def create(self, validated_data):
+    def create(self, **validated_data):
         transaction = Transaction.objects.create(**validated_data)
         return transaction
+
+    def save(self, **validated_data):
+        user = VirtualWalletUser.objects.get(pk=validated_data['owner'].id)
+        wallet = user.wallet
+        wallet.balance += Decimal(self.initial_data['value'])
+        wallet.save()
+        transaction = Transaction.objects.create(wallet=wallet,
+                                                 email=user.email,
+                                                 type='payment_fill',
+                                                 value=Decimal(self.initial_data['value']))
+        return transaction
+
+    @staticmethod
+    def validate_value(value):
+        if Decimal(value) <= 0:
+            raise serializers.ValidationError("You have to fill wallet with amount bigger then 0")
 
 
 class WithdrawSerializer(serializers.ModelSerializer):
@@ -26,6 +46,24 @@ class WithdrawSerializer(serializers.ModelSerializer):
         transaction = Transaction.objects.create(**validated_data)
         return transaction
 
+    def save(self, **validated_data):
+        user = VirtualWalletUser.objects.get(pk=validated_data['owner'].id)
+        wallet = user.wallet
+        if wallet.balance < Decimal(self.initial_data['value']):
+            raise serializers.ValidationError("Sorry, you don't have enough money in your wallet")
+        wallet.balance -= Decimal(self.initial_data['value'])
+        wallet.save()
+        transaction = Transaction.objects.create(wallet=wallet,
+                                                 email=user.email,
+                                                 type='payment_withdraw',
+                                                 value=Decimal(self.initial_data['value']))
+        return transaction
+
+    @staticmethod
+    def validate_value(value):
+        if Decimal(value) <= 0:
+            raise serializers.ValidationError("You have to withdraw with amount bigger then 0")
+
 
 class PaySerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,6 +73,50 @@ class PaySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         transaction = Transaction.objects.create(**validated_data)
         return transaction
+
+    def save(self, **validated_data):
+        payer = VirtualWalletUser.objects.get(pk=validated_data['owner'].id)
+        wallet = payer.wallet
+        if wallet.balance < Decimal(self.initial_data['value']):
+            raise serializers.ValidationError("Sorry, you don't have enough money in your wallet")
+        else:
+            with atomic():
+                payer_wallet = payer.wallet
+                receiver = VirtualWalletUser.objects.get(email=self.initial_data['email'])
+                receiver_wallet = receiver.wallet
+
+                payer_wallet.balance -= Decimal(self.initial_data['value'])
+                receiver_wallet.balance += Decimal(self.initial_data['value'])
+
+                payer_wallet.save()
+                receiver_wallet.save()
+
+                payer_transaction = Transaction.objects.create(
+                    wallet=payer_wallet,
+                    email=payer.email,
+                    type='payment_made',
+                    value=Decimal(self.initial_data['value'])
+                )
+
+                receiver_transaction = Transaction.objects.create(
+                    wallet=receiver_wallet,
+                    email=receiver.email,
+                    type='payment_received',
+                    value=Decimal(self.initial_data['value'])
+                )
+
+                payer_transaction.save()
+                receiver_transaction.save()
+
+    @staticmethod
+    def validate_value(value):
+        if Decimal(value) <= 0:
+            raise serializers.ValidationError("You have to define payment value bigger then 0")
+
+    @staticmethod
+    def validate_email(email):
+        if not VirtualWalletUser.objects.filter(email=email):
+            raise serializers.ValidationError("There is no payment receiver with such email. Please, check it")
 
 
 class TransactionSerializer(serializers.Serializer):
--- transactions/views.py
+++ transactions/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
+from django.db.transaction import atomic
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.response import Response
-from django.db.transaction import atomic
-from django.core.exceptions import ObjectDoesNotExist
-
-from decimal import Decimal, getcontext
 
 from .serializers import FillSerializer, WithdrawSerializer, PaySerializer, TransactionSerializer
 from .models import Transaction, Wallet
-from user.models import VirtualWalletUser
-
-
-from pprint import pprint
-# class IsOwnerOrReadOnly(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return obj.owner == request.user
-
-getcontext().prec = 2
 
 
 class FillWalletView(generics.CreateAPIView):
@@ -25,22 +14,11 @@ class FillWalletView(generics.CreateAPIView):
     serializer_class = FillSerializer
 
     def post(self, request, *args, **kwargs):
-        user = VirtualWalletUser.objects.get(id=request.user.id)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        transaction = serializer.save(email=user.email, type='payment_fill', wallet=user.wallet)
+        transaction = serializer.save(owner=request.user)
         transaction.save()
-
-        wallet = transaction.wallet
-        wallet.balance += Decimal(
-            FillSerializer(transaction, context=self.get_serializer_context()).data['value']
-        )
-
-        wallet.save()
         return Response(data={'response': 'Successful fill'}, status=200)
-        # return Response({
-        #     "Value": FillSerializer(transaction, context=self.get_serializer_context()).data['value'],
-        # })
 
 
 class WithdrawWalletView(generics.CreateAPIView):
@@ -48,25 +26,11 @@ class WithdrawWalletView(generics.CreateAPIView):
     serializer_class = WithdrawSerializer
 
     def post(self, request, *args, **kwargs):
-        user = VirtualWalletUser.objects.get(id=request.user.id)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        if user.wallet.balance < Decimal(request.data['value']):
-            return Response(data={'response': "Sorry, you don't have enough money in your wallet"}, status=200)
-        else:
-            transaction = serializer.save(email=user.email, type='payment_withdraw', wallet=user.wallet)
-            transaction.save()
-            # wallet = transaction.wallet
-
-            user.wallet.balance -= Decimal(
-                FillSerializer(transaction, context=self.get_serializer_context()).data['value']
-            )
-            user.wallet.save()
-            return Response(data={'response': 'Successful withdraw'}, status=200)
-        # return Response({
-        #     "Value": FillSerializer(transaction, context=self.get_serializer_context()).data['value'],
-        # })
+        transaction = serializer.save(owner=request.user)
+        transaction.save()
+        return Response(data={'response': 'Successful withdraw'}, status=200)
 
 
 class PayWalletView(generics.CreateAPIView):
@@ -74,37 +38,10 @@ class PayWalletView(generics.CreateAPIView):
     serializer_class = PaySerializer
 
     def post(self, request, *args, **kwargs):
-        user = VirtualWalletUser.objects.get(id=request.user.id)
-
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        payer_wallet = user.wallet
-
-        if payer_wallet.balance < Decimal(request.data['value']):
-            return Response(data={'response': "Sorry, you don't have enough money in your wallet"}, status=200)
-        else:
-            try:
-                receiver = VirtualWalletUser.objects.get(email=request.data['email'])
-            except ObjectDoesNotExist:
-                return Response(data={'response': "There is no receiver with such email"}, status=200)
-            with atomic():
-                payer_wallet.balance -= Decimal(request.data['value'])
-                receiver_wallet = receiver.wallet
-                receiver_wallet.balance += Decimal(request.data['value'])
-
-                payer_wallet.save()
-                receiver_wallet.save()
-
-                payer_transaction = serializer.save(wallet=payer_wallet, type='payment_made')
-                receiver_transaction = Transaction.objects.create(
-                    wallet=receiver_wallet,
-                    email=receiver_wallet.owner,
-                    type='payment_received',
-                    value=Decimal(request.data['value'])
-                )
-
-                return Response(data={'response': "Successful payment"}, status=200)
+        serializer.save(owner=request.user)
+        return Response(data={'response': 'Successful payment'}, status=200)
 
 
 class TransactionsView(generics.ListAPIView):
@@ -113,7 +50,6 @@ class TransactionsView(generics.ListAPIView):
 
     def get_queryset(self):
         user_wallet = Wallet.objects.get(owner=self.request.user)
-        pprint(f"{user_wallet=}")
         users_transactions = Transaction.objects.filter(
             wallet=user_wallet,
             date__lte=self.request.GET['start_date'],
